a69ee1839c5f7a991cfc0f283f6fceeb4ab7b16e
Nicole Daphne C. Ong <nicole_daphne_ong@dlsu.edu.ph>
2024-07-26 11:02:15 +0800

    Add Comments for extension.js
    Add comments for extension.js

--- client/out/extension.js
+++ client/out/extension.js
@@ -1,3 +1,6 @@
+/*
+	extension.js sets up the language server to provide language features (auto completion, linting etc.) for HTML, CSS, JS.
+*/
 'use strict';
 /* --------------------------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
@@ -9,6 +12,12 @@ const path = require('path');
 const vscode_1 = require('vscode');
 const node_1 = require('vscode-languageclient/node');
 let client;
+
+/*
+	Called when the extension is activated. Path to the server module (server.js) is determined using context.asAbsolutePath.
+	Debug options are specified to let the server run in Node Inspector mode.
+	serverOptions object defines how server will run in both normal and debug.
+*/
 function activate(context) {
 	// The server is implemented in node
 	const serverModule = context.asAbsolutePath(path.join('server', 'out', 'server.js'));
@@ -34,7 +43,7 @@ function activate(context) {
 			{ scheme: 'file', language: 'css' },
 		],
 		synchronize: {
-			// Watching the active editor for file changes (source: https://code.visualstudio.com/api/references/vscode-api#workspace.createFileSystemWatcher)
+			// Watching the active editor for file changes to synchronize server with current state of the file (source: https://code.visualstudio.com/api/references/vscode-api#workspace.createFileSystemWatcher)
 			fileEvents: vscode_1.workspace.createFileSystemWatcher(
 				new vscode_1.RelativePattern(vscode_1.window.activeTextEditor.document.uri, '*')
 			),
@@ -50,12 +59,19 @@ function activate(context) {
 	// Start the client. This will also launch the server
 	client.start();
 }
+
+// Allows activate to be called by VSCode when extension activates, standard practice.
 exports.activate = activate;
+
+// Clean up resources if extension is deactivated
 function deactivate() {
+	// Does nothing if no client defined
 	if (!client) {
 		return undefined;
 	}
 	return client.stop();
 }
+
+// Allows deactivate to be called by VSCode when extension deactivates, standard practice.
 exports.deactivate = deactivate;
 //# sourceMappingURL=extension.js.map
