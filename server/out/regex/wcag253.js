e877284ca3a8a9bb677ca0b3e19771183bc21783
marcojalenyu <112309140+marcojalenyu@users.noreply.github.com>
2024-09-05 20:57:36 +0800

    Separated all WCAG 2.5.3 regex to one file

---
+++ server/out/regex/wcag253.js
@@ -0,0 +1,145 @@
+/**
+ * @summary Checks if HTML follows WCAG Success Critereon 2.5.3 - Label in Name
+ */
+const { DiagnosticSeverity } = require("vscode-languageserver/node");
+
+// A. Button elements must have an aria-label or aria-labelledby attribute
+const pattern253a = /<button(?![^>]*(?:aria-label|aria-labelledby)[^>]*)(?:(?<=\sname=)[^>\s]+)?(?:(?<=\svalue=)['"][^'"]*['"])?(?:\s+aria-label=(?:""|''))?[^>]*>/g;
+// B. Input elements must have an aria-label or aria-labelledby attribute
+const pattern253b = /<input(?![^>]*(?:aria-label|aria-labelledby)[^>]*)(?:(?<=\sname=)[^>\s]+)?(?:(?<=\svalue=)['"][^'"]*['"])?(?:\s+aria-label=(?:""|''))?[^>]*>/g;
+// C. Textarea elements must have an aria-label or aria-labelledby attribute
+const pattern253c = /<textarea(?![^>]*(?:aria-label|aria-labelledby)[^>]*)(?:(?<=\sname=)[^>\s]+)?(?:(?<=\svalue=)['"][^'"]*['"])?(?:\s+aria-label=(?:""|''))?[^>]*>/g;
+// D. Select elements must have an aria-label or aria-labelledby attribute
+const pattern253d = /<select(?![^>]*(?:aria-label|aria-labelledby)[^>]*)(?:(?<=\sname=)[^>\s]+)?(?:(?<=\svalue=)['"][^'"]*['"])?(?:\s+aria-label=(?:""|''))?[^>]*>/g;
+
+// Check WCAG 2.5.3
+function checkWCAG253(m, text, textDocument, problems, diagnostics, settings, hasDiagnosticRelatedInformationCapability) {
+	checkWCAG253a(m, text, textDocument, problems, diagnostics, settings, hasDiagnosticRelatedInformationCapability);
+	checkWCAG253b(m, text, textDocument, problems, diagnostics, settings, hasDiagnosticRelatedInformationCapability);
+	checkWCAG253c(m, text, textDocument, problems, diagnostics, settings, hasDiagnosticRelatedInformationCapability);
+	checkWCAG253d(m, text, textDocument, problems, diagnostics, settings, hasDiagnosticRelatedInformationCapability);
+}
+
+// Check pattern 2.5.3a
+function checkWCAG253a(m, text, textDocument, problems, diagnostics, settings, hasDiagnosticRelatedInformationCapability) {
+	while ((m = pattern253a.exec(text)) && problems < settings.maxNumberOfProblems) {
+		problems++;
+		const diagnostic = {
+		  severity: DiagnosticSeverity.Warning,
+		  range: {
+			start: textDocument.positionAt(m.index),
+			end: textDocument.positionAt(m.index + m[0].length),
+		  },
+		  message: `The button element needs more context.`,
+		  source: "WCAG 2.1 | 2.5.3",
+		};
+		if (hasDiagnosticRelatedInformationCapability) {
+		  diagnostic.relatedInformation = [
+			{
+			  location: {
+				uri: textDocument.uri,
+				range: Object.assign({}, diagnostic.range),
+			  },
+			  message:
+				'Add the appropriate aria-label to the input element.',
+			},
+		  ];
+		}
+		diagnostics.push(diagnostic);
+	}
+}
+
+// Check pattern 2.5.3b
+function checkWCAG253b(m, text, textDocument, problems, diagnostics, settings, hasDiagnosticRelatedInformationCapability) {
+	while (
+		(m = pattern253b.exec(text)) &&
+		problems < settings.maxNumberOfProblems
+	  ) {
+		problems++;
+		const diagnostic = {
+		  severity: DiagnosticSeverity.Warning,
+		  range: {
+			start: textDocument.positionAt(m.index),
+			end: textDocument.positionAt(m.index + m[0].length),
+		  },
+		  message: `The input element needs more context.`,
+		  source: "WCAG 2.1 | 1.3.1",
+		};
+		if (hasDiagnosticRelatedInformationCapability) {
+		  diagnostic.relatedInformation = [
+			{
+			  location: {
+				uri: textDocument.uri,
+				range: Object.assign({}, diagnostic.range),
+			  },
+			  message: "Add the appropriate aria-label to the input element.",
+			},
+		  ];
+		}
+		diagnostics.push(diagnostic);
+	}
+}
+
+// Check pattern 2.5.3c
+function checkWCAG253c(m, text, textDocument, problems, diagnostics, settings, hasDiagnosticRelatedInformationCapability) {
+	while (
+		(m = pattern253c.exec(text)) &&
+		problems < settings.maxNumberOfProblems
+	) {
+		problems++;
+		const diagnostic = {
+		  severity: DiagnosticSeverity.Warning,
+		  range: {
+			start: textDocument.positionAt(m.index),
+			end: textDocument.positionAt(m.index + m[0].length),
+		  },
+		  message: `The textarea element needs more context.`,
+		  source: "WCAG 2.1 | 1.3.1",
+		};
+		if (hasDiagnosticRelatedInformationCapability) {
+		  diagnostic.relatedInformation = [
+			{
+			  location: {
+				uri: textDocument.uri,
+				range: Object.assign({}, diagnostic.range),
+			  },
+			  message: "Add the appropriate aria-label to the textarea element.",
+			},
+		  ];
+		}
+		diagnostics.push(diagnostic);
+	}
+}
+
+// Check pattern 2.5.3d
+function checkWCAG253d(m, text, textDocument, problems, diagnostics, settings, hasDiagnosticRelatedInformationCapability) {
+	while (
+		(m = pattern253d.exec(text)) &&
+		problems < settings.maxNumberOfProblems
+	) {
+		problems++;
+		const diagnostic = {
+		  severity: DiagnosticSeverity.Warning,
+		  range: {
+			start: textDocument.positionAt(m.index),
+			end: textDocument.positionAt(m.index + m[0].length),
+		  },
+		  message: `The select element needs more context.`,
+		  source: "WCAG 2.1 | 1.3.1",
+		};
+		if (hasDiagnosticRelatedInformationCapability) {
+		  diagnostic.relatedInformation = [
+			{
+			  location: {
+				uri: textDocument.uri,
+				range: Object.assign({}, diagnostic.range),
+			  },
+			  message: "Add the appropriate aria-label to the select element.",
+			},
+		  ];
+		}
+		diagnostics.push(diagnostic);
+	}
+}
+
+module.exports = checkWCAG253;
