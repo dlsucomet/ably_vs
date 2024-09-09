4fd87531c4225b2b6a8dec9cf1ddc59859a8a3a3
marcojalenyu <112309140+marcojalenyu@users.noreply.github.com>
2024-09-09 15:50:50 +0800

    Update img-caption.js

--- server/out/helpers/img-caption.js
+++ server/out/helpers/img-caption.js
@@ -18,7 +18,7 @@ async function suggestAltText(imgPath) {
       imgData = fs.readFileSync(imgPath);
     }
     // Get the token from the environment
-	const token = process.env.BLIP_TOKEN;
+	  const token = process.env.BLIP_TOKEN;
     const response = await fetch(
         "https://api-inference.huggingface.co/models/Salesforce/blip-image-captioning-large",
         {
@@ -30,23 +30,23 @@ async function suggestAltText(imgPath) {
             body: imgData,
         }
     );
-	// Get the response from the API
+	  // Get the response from the API
     const result = await response.json();
-	// Get the generated text from the response and format it
+	  // Get the generated text from the response and format it
     let generatedCaption = result[0].generated_text;
-	generatedCaption = generatedCaption
-		.replace("there are ", "")
-		.replace("there is ", "")
-		.replace("arafed ", "")
-		.replace("araffe ", "")
-		.replace("araf ", "person")
-		.replace("arafe ", "")
-		.replace("araffes ", "people ")
-		.replace("araffy ", "")
-    .replace("araffed ", "")
-	generatedCaption = generatedCaption.charAt(0).toUpperCase() + generatedCaption.slice(1);
-	// Return the alt text as a string of the img tag
-	return `: <img src='${imgPath}' alt='${generatedCaption}'>`;
+	  generatedCaption = generatedCaption
+      .replace("there are ", "")
+      .replace("there is ", "")
+      .replace("arafed ", "")
+      .replace("araffe ", "")
+      .replace("araf ", "person")
+      .replace("arafe ", "")
+      .replace("araffes ", "people ")
+      .replace("araffy ", "")
+      .replace("araffed ", "")
+	  generatedCaption = generatedCaption.charAt(0).toUpperCase() + generatedCaption.slice(1);
+	  // Return the alt text as a string of the img tag
+	  return `: <img src='${imgPath}' alt='${generatedCaption}'>`;
   } catch (error) {
     // If the image is not supported
     return `. Unfortunately, we cannot recommend an alt-text for this image.`
